f91428dc084eafa4a14e9de380147d2035398d01
Mareike Picklum <mareikep@cs.uni-bremen.de>
2023-05-26 14:47:22 +0200

    minor fix

--- test/test_astar.py
+++ test/test_astar.py
@@ -102,7 +102,7 @@ class AStarAlgorithmTests(unittest.TestCase):
         self.assertEqual(self.path, [(0, 0), (0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (2, 4), (3, 4), (4, 4), (5, 4), (5, 5), (5, 6), (6, 6)], msg='A* path incorrect')
 
     def test_bdir_astar_path(self) -> None:
-        bidir_astar = BiDirAStar(SubAStar, AStarAlgorithmTests.init, AStarAlgorithmTests.goal)
+        bidir_astar = BiDirAStar(SubAStar, SubAStar, AStarAlgorithmTests.init, AStarAlgorithmTests.goal)
         self.path = bidir_astar.search()
 
         self.assertEqual(self.path, [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4), (5, 4), (5, 5), (5, 6), (6, 6)], msg='Bi-directional A* path incorrect')
